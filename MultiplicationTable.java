2f33b0275dc4153e2bf9e4f1e73e4e87fdd299d6
ayushkumar1100 <ayushkumar4010@gmail.com>
2026-04-05 01:59:09 +0530

    done

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,8 +1,9 @@
 class MultiplicationTable{
     public static void main(String[] args){
-        int num = 5;
         for(int i=1; i<=10; i++){
-            System.out.println(num + "x" + i + "=" + (num * i));
+            int num = 5;
+            System.out.println(num + " x " + i + " = " + (num * i));
         }
     }
+
 }
