73a112558faabf13f9ca81525a36b5c56f62f30c
ayushkumar1100 <ayushkumar4010@gmail.com>
2026-04-05 22:09:33 +0530

    sum of enterd number in command line args during execution

--- CommandLineDemo.java
+++ CommandLineDemo.java
@@ -1,8 +1,16 @@
 class CommandLineDemo{
     public static void main(String[] args){
-        System.out.println("First argument: " + args[0]);
-        System.out.println("Second argument: " + args[1]);
-        System.out.println("third argument: " + args[2]);
+        int n = args.length;
+        int sum = 0;
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = Integer.parseInt(args[i]);
+            sum += arr[i];
+        }
+        System.out.println("The sum of the number entered by you during Command Line Execution is: " + sum);
+        //System.out.println("First argument: " + args[0]);
+        //System.out.println("Second argument: " + args[1]);
+        //System.out.println("third argument: " + args[2]);
     }
 }
 
