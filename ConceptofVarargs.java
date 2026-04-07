0748114519431c014eba1e1de9b4a5d57c9e463d
ayushkumar1100 <ayushkumar4010@gmail.com>
2026-04-08 01:28:22 +0530

    by this program i showcase the cocept of varargs with for each loop.

---
+++ ConceptofVarargs.java
@@ -0,0 +1,23 @@
+class SumNum{
+   void print(double...arr){    // varargs method to accept variable number of arguments and internally it will be treated as an array
+        
+        double sum=0;         
+        for(double i:arr) sum+=i;
+        
+        
+        System.out.println((char)arr[arr.length-1]);  // printing the last element of the array which is a character in this case
+        System.out.println("The sum is "+sum);
+    }
+}
+class ConceptofVarargs{
+    public static void main(String[] args)
+    {
+       SumNum obj = new SumNum();
+       obj.print('a');
+       obj.print(1,'a');
+       obj.print('a',1,2.2,'a');
+       obj.print('a',1,2,3.4,'a');
+       obj.print('a',1,2,3.5,4,5.5,'a');
+       
+    }
+}
